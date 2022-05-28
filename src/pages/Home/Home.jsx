0089f24124d6df7fcec3008f836aa0f0679b29a7
Kykal <benavides.alan@hotmail.com>
2022-05-28 18:07:05 -0500

    Added media queries

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,25 @@
 import React, { Suspense, lazy } from 'react';
 
 //MATERIAL DESIGN
-//Components
-import Box from '@mui/material/Box';
-
+//Hooks
+import { useMediaQuery } from '@mui/material'; //Media queries
 
 //Custom components
 import Header from '../../components/Header';
+import HeaderMobile from '../../components/HeaderMobile';
 
 
 //Main component content
 const Home = () => {
 
+	//Is bigger than 850px?
+	const isDesktop = useMediaQuery('(min-width: 768px)');
+
 	//Component render
 	return (
 		<>
-			<Header />
+			{  isDesktop && <Header /> }
+			{ !isDesktop && <HeaderMobile /> }
 			<main>
 				<span style={{ color: "white" }}>Texto cualquiera</span>
 			</main>
