3db89da25c01032e0ce512778d37605619c803f9
Kykal <benavides.alan@hotmail.com>
2022-05-30 17:45:58 -0500

    Export everything (*) from Header folder

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,3 @@
-export { default as Header				} from './Header';
-export { default as LoadingScreen	} from './LoadingScreen';
-export { default as SearchBar			} from './SearchBar';
+export * 										from './Header';
+export { default as LoadingScreen	}	from './LoadingScreen';
+export { default as SearchBar			}	from './SearchBar';
