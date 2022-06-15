0c81e6f976e857b9497c583cf7849d408db65f2d
Kykal <benavides.alan@hotmail.com>
2022-06-15 00:17:28 -0500

    Filters, filters labels and states
    - Filters are now rendered
    - Filters now can be checked
    - Filters labels are sliced in two sections

--- src/components/ProductsList/ProductsListMobile.jsx
+++ src/components/ProductsList/ProductsListMobile.jsx
@@ -1,23 +1,26 @@
 import React, { useState, lazy, Suspense } from 'react';
 
+//Translations
+import { useTranslation } from 'react-i18next';
+
 
 //MATERIAL DESIGN
 //Components
 import Grid						from '@mui/material/Grid';
+import List						from '@mui/material/List';
+import ListItem				from '@mui/material/ListItem';
+import ListItemButton		from '@mui/material/ListItemButton';
+import ListItemIcon			from '@mui/material/ListItemIcon';
+import ListItemText			from '@mui/material/ListItemText';
+import Checkbox				from '@mui/material/Checkbox';
 import IconButton				from '@mui/material/IconButton';
 import ImageList				from '@mui/material/ImageList';
-import ImageListItem			from '@mui/material/ImageListItem';
-import ImageListItemBar		from '@mui/material/ImageListItemBar';
-import ListSubheader			from '@mui/material/ListSubheader';
 import Typography				from '@mui/material/Typography';
 //Icons
 import FilterListIcon		from '@mui/icons-material/FilterList';
 import FilterListOffIcon	from '@mui/icons-material/FilterListOff';
-import InfoIcon				from '@mui/icons-material/Info';
 //Styles
 import { styled }				from '@mui/material/styles';
-
-
 
 
 //Custom components
@@ -33,13 +36,39 @@ const StyledButton = styled(IconButton)({
 
 
 //Main component content
-const ProductsListMobile = ({ products, filtersLabels, label, language }) => {
+const ProductsListMobile = ({ products, filteredProducts, filtersLabels, label, language }) => {
 
+	//Translation
+	const [ t ] = useTranslation("global");
+
+
+	const [ activeFilters, setActiveFilters ] = useState([]);
 	const [ showFilters, setShowFilters ] = useState(false);
 
-	//Description. What does this?
+	//Renders filters list
 	const filterButtonHandler = () => {
 		setShowFilters( !showFilters );
+	};
+
+	const filtersLabelsHalfLength = Math.ceil(filtersLabels.length/2); //Get a half number of array
+	const filtersFirstHalf	= filtersLabels.slice( 0, filtersLabelsHalfLength ); //Slice from beginning to half
+	const filtersSecondHalf	= filtersLabels.slice( -(filtersLabelsHalfLength-1) ); //Slice the rest (from half to end)
+
+	
+
+
+	//Manages checkbox states
+	const filtersToggleHandler = (value) => () => {
+		const currentIndex = activeFilters.indexOf(value);
+		const newChecked = [...activeFilters];
+
+		if( currentIndex === -1 ){
+			newChecked.push(value);
+		}else{
+			newChecked.splice(currentIndex, 1);
+		}
+
+		setActiveFilters(newChecked);
 	};
 
 	//Component render
@@ -55,8 +84,60 @@ const ProductsListMobile = ({ products, filtersLabels, label, language }) => {
 					</StyledButton>
 				</Grid>
 				{showFilters &&				
-					<Grid item container xs={12}>
-						Filters
+					<Grid item container xs={12} spacing={0}>
+						<Grid item xs={12} >
+							<Typography variant="subtitle1" >{t("manufacturer")}</Typography>
+						</Grid>
+						<Grid item xs={6}>
+							<List>
+								{filtersFirstHalf.map( (filter, index) => (
+									<ListItem key={index} disablePadding >
+										<ListItemButton role={undefined} dense onClick={ filtersToggleHandler(index) } >
+											<ListItemIcon>
+												<Checkbox
+													edge="start"
+													checked={activeFilters.indexOf(index) !== -1}
+													tabIndex={-1}
+													disableRipple
+													sx={{
+														color: "var(--white-2)",
+														"&.Mui-checked": {
+															color: "var(--magenta)"
+														}
+													}}
+												/>
+											</ListItemIcon>
+											<ListItemText primary={filter} />
+										</ListItemButton>
+									</ListItem>
+								) )}
+							</List>
+						</Grid>
+						<Grid item xs={6}>
+							<List>
+								{filtersSecondHalf.map( (filter, index) => (
+									<ListItem key={index} disablePadding >
+										<ListItemButton role={undefined} dense onClick={ filtersToggleHandler((index+filtersLabelsHalfLength)) } >
+											<ListItemIcon>
+												<Checkbox
+													edge="start"
+													checked={ activeFilters.indexOf((index+filtersLabelsHalfLength)) !== -1 }
+													tabIndex={-1}
+													disableRipple
+													sx={{
+														color: "var(--white-2)",
+														"&.Mui-checked": {
+															color: "var(--magenta)"
+														}
+													}}
+												/>
+											</ListItemIcon>
+											<ListItemText primary={filter} />
+										</ListItemButton>
+									</ListItem>
+								) )}
+							</List>
+						</Grid>
 					</Grid>
 				}
 			</Grid>
