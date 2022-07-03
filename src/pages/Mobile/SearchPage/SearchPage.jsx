bde480348c991a30a48afc87894ca5b7a36fcdbe
Kykal <benavides.alan@hotmail.com>
2022-07-02 23:47:10 -0500

    Search page created

---
+++ src/pages/Mobile/SearchPage/SearchPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+//MATERIAL DESIGN
+//Components
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+//Main component content
+const SearchPage = () => {
+
+
+	//Component render
+	return (
+		<>
+			<Box display="flex" justifyContent="flex-start" alignItems="center" >
+				<Typography	pography variant="h5" >Search results</Typography>
+			</Box>
+		</>
+	);
+};
+
+
+export default SearchPage; //Export main component
